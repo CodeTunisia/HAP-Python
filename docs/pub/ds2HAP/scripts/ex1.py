15a7ef0602f4785b5978cf7af08f2043df3ec14a
astrax <ahmed.ammar@fst.utm.tn>
2021-03-10 14:09:41 +0100

    optique

--- docs/pub/ds2HAP/scripts/ex1.py
+++ docs/pub/ds2HAP/scripts/ex1.py
@@ -3,7 +3,16 @@
 """
 Created on Wed Feb  3 13:30:41 2021
 
-@author: ahmed
+@author: ahmed 
+Automate cellulaire
 """
 
+def grille(n):
+    M = []
+    for i in range(n):
+        L =[]
+        for j in range(n):
+            L.append(0)
+        M.append(L)
+    return M
 
